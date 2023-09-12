a29bd271d6063900f8a404c35310f07de015cefd
Brandon Knight <knightbrandon1984@gmail.com>
2023-09-12 11:27:28 -0500

    Roll dice works, and displays currently only on P1

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
 'use strict';
 
-document.querySelector('.btn--new');
+// Selecting Elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
-document.querySelector('.btn--roll');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  //    -when I click I need it to roll a random number 1-6
+  //    -the number correlates to the 6 dice images
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  // 2. Display dice
+  //    -to display I will need to remove the hidden class
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  // 3. Check for rolled 1: if true, switch to next player
+  // switch to player2
+
+  if (dice !== 1) {
+    // Add dice to current score
+    // currentScore = currentScore + dice;
+    // This shorthand does the same thing...
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    // Switch to next player
+  }
+});
